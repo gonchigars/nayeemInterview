042ab45716181987023948f725c76f2665d58e05
ratan-17 <rkmule2002@gmail.com>
2024-09-11 12:57:11 +0530

    ratan

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -21,7 +21,7 @@ public class UserController {
     @PostMapping("/register")
     public ResponseEntity<UserResponseDTO> registerUser(@RequestBody UserDTO userDTO) {
         // Convert UserDTO to User entity
-        User user = new User(userDTO.getEmail(), userDTO.getPassword(), userDTO.getRole());
+        User user = new User(userDTO.getEmail(),  userDTO.getRole());
 
         // Register the user and get the saved User entity
         User registeredUser = userService.registerUser(user);
@@ -35,15 +35,15 @@ public class UserController {
         return ResponseEntity.ok(userResponseDTO);
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<String> loginUser(@RequestBody LoginRequestDTO loginRequestDTO) {
-        boolean isAuthenticated = userService.authenticate(loginRequestDTO.getEmail(), loginRequestDTO.getPassword());
-        if (isAuthenticated) {
-            return ResponseEntity.ok("Login successful");
-        } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
-        }
-    }
+    // @PostMapping("/login")
+    // public ResponseEntity<String> loginUser(@RequestBody LoginRequestDTO loginRequestDTO) {
+    //     boolean isAuthenticated = userService.authenticate(loginRequestDTO.getEmail(), loginRequestDTO.getPassword());
+    //     if (isAuthenticated) {
+    //         return ResponseEntity.ok("Login successful");
+    //     } else {
+    //         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+    //     }
+    // }
 
     @GetMapping("/{id}")
     public ResponseEntity<UserResponseDTO> getUserById(@PathVariable Long id) {
